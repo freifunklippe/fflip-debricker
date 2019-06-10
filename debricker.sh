bb13b745f483639ef99032e3cb4296aff89a5ffb
Michael Brinkmann <michael@collimas.net>
2019-06-10 11:56:05 +0200

    Added devices

--- debricker.sh
+++ debricker.sh
@@ -5,12 +5,253 @@
 # Rescue Script to debrick router devices
 # Created by Collimas (mb@freifunk-lippe.de)
 # License: GPLv3
-# Requirements: tftp-hpa package
+# Requirements: tftp-hpa & unzip packages
 #
 
 # Variables
 
 # Functions
+
+wr841v8ff() {
+ifconfig eth0:0 192.168.0.66 netmask 255.255.255.0 up
+systemctl restart tftpd-hpa
+rm -rf /tmp/*
+rm -rf /srv/tftp/*.bin
+mkdir -p /tmp/wr841temp
+cd /tmp/wr841temp
+wget "https://wizard.freifunk-lippe.de/images/stable/factory/gluon-fflip-1.1.3-20190502-tp-link-tl-wr841n-nd-v8.bin"
+echo "Freifunk-Firmware erfolgreich heruntergeladen"
+cp gluon-fflip*.bin /srv/tftp/wr841nv8_tp_recovery.bin
+echo
+echo "1. Verbinden Sie nun die blaue WAN-Buchse des Routers mit dem Netzwerk."
+echo "2. Schalten Sie den Router aus."
+echo "3. Schalten Sie den Router bei gedrückter Reset-Taste ein."
+echo "4. Warten Sie ca. 5 Sekunden bis Sie die Reset-Taste loslassen."
+echo "5. Der Router sollte nach ca. 1 Minute neu starten und funktionsfähig sein."
+echo
+read -p "Bitte ENTER drücken um fortzufahren."
+clear
+auswahl
+}
+
+wr841v8stock() {
+ifconfig eth0:0 192.168.0.66 netmask 255.255.255.0 up
+systemctl restart tftpd-hpa
+rm -rf /tmp/*
+rm -rf /srv/tftp/*.bin
+mkdir -p /tmp/wr841temp
+cd /tmp/wr841temp
+wget "https://static.tp-link.com/TL-WR841N(UN)_V8_170210.zip"
+unzip -j TL-WR841*.zip
+rm TL-WR841*.zip
+echo "Hersteller-Firmware erfolgreich heruntergeladen und entpackt"
+mv wr841*.bin orig.bin
+dd if=orig.bin of=wr841nv8_tp_recovery.bin skip=257 bs=512
+cp wr841nv8_tp_recovery.bin /srv/tftp/
+echo
+echo "1. Verbinden Sie nun die blaue WAN-Buchse des Routers mit dem Netzwerk."
+echo "2. Schalten Sie den Router aus."
+echo "3. Schalten Sie den Router bei gedrückter Reset-Taste drücken) ein."
+echo "4. Warten Sie ca. 5 Sekunden bis Sie die Reset-Taste loslassen."
+echo "5. Der Router sollte nach ca. 1 Minute neu starten und funktionsfähig sein."
+echo
+read -p "Bitte ENTER drücken um fortzufahren."
+clear
+auswahl
+}
+
+wr841v9ff() {
+ifconfig eth0:0 192.168.0.66 netmask 255.255.255.0 up
+systemctl restart tftpd-hpa
+rm -rf /tmp/*
+rm -rf /srv/tftp/*.bin
+mkdir -p /tmp/wr841temp
+cd /tmp/wr841temp
+wget "https://wizard.freifunk-lippe.de/images/stable/factory/gluon-fflip-1.1.3-20190502-tp-link-tl-wr841n-nd-v9.bin"
+echo "Freifunk-Firmware erfolgreich heruntergeladen"
+cp gluon-fflip*.bin /srv/tftp/wr841nv9_tp_recovery.bin
+echo
+echo "1. Verbinden Sie nun die blaue WAN-Buchse des Routers mit dem Netzwerk."
+echo "2. Schalten Sie den Router aus."
+echo "3. Schalten Sie den Router bei gedrückter Reset-Taste ein."
+echo "4. Warten Sie ca. 5 Sekunden bis Sie die Reset-Taste loslassen."
+echo "5. Der Router sollte nach ca. 1 Minute neu starten und funktionsfähig sein."
+echo
+read -p "Bitte ENTER drücken um fortzufahren."
+clear
+auswahl
+}
+
+wr841v9stock() {
+ifconfig eth0:0 192.168.0.66 netmask 255.255.255.0 up
+systemctl restart tftpd-hpa
+rm -rf /tmp/*
+rm -rf /srv/tftp/*.bin
+mkdir -p /tmp/wr841temp
+cd /tmp/wr841temp
+wget "https://static.tp-link.com/res/down/soft/TL-WR841N_V9_150310.zip"
+unzip -j TL-WR841*.zip
+rm TL-WR841*.zip
+echo "Hersteller-Firmware erfolgreich heruntergeladen und entpackt"
+mv wr841*.bin orig.bin
+dd if=orig.bin of=wr841nv9_tp_recovery.bin skip=257 bs=512
+cp wr841nv9_tp_recovery.bin /srv/tftp/
+echo
+echo "1. Verbinden Sie nun die blaue WAN-Buchse des Routers mit dem Netzwerk."
+echo "2. Schalten Sie den Router aus."
+echo "3. Schalten Sie den Router bei gedrückter Reset-Taste drücken) ein."
+echo "4. Warten Sie ca. 5 Sekunden bis Sie die Reset-Taste loslassen."
+echo "5. Der Router sollte nach ca. 1 Minute neu starten und funktionsfähig sein."
+echo
+read -p "Bitte ENTER drücken um fortzufahren."
+clear
+auswahl
+}
+
+wr841v10ff() {
+ifconfig eth0:0 192.168.0.66 netmask 255.255.255.0 up
+systemctl restart tftpd-hpa
+rm -rf /tmp/*
+rm -rf /srv/tftp/*.bin
+mkdir -p /tmp/wr841temp
+cd /tmp/wr841temp
+wget "https://wizard.freifunk-lippe.de/images/stable/factory/gluon-fflip-1.1.3-20190502-tp-link-tl-wr841n-nd-v10.bin"
+echo "Freifunk-Firmware erfolgreich heruntergeladen"
+cp gluon-fflip*.bin /srv/tftp/wr841nv10_tp_recovery.bin
+echo
+echo "1. Verbinden Sie nun die blaue WAN-Buchse des Routers mit dem Netzwerk."
+echo "2. Schalten Sie den Router aus."
+echo "3. Schalten Sie den Router bei gedrückter Reset-Taste ein."
+echo "4. Warten Sie ca. 5 Sekunden bis Sie die Reset-Taste loslassen."
+echo "5. Der Router sollte nach ca. 1 Minute neu starten und funktionsfähig sein."
+echo
+read -p "Bitte ENTER drücken um fortzufahren."
+clear
+auswahl
+}
+
+wr841v10stock() {
+ifconfig eth0:0 192.168.0.66 netmask 255.255.255.0 up
+systemctl restart tftpd-hpa
+rm -rf /tmp/*
+rm -rf /srv/tftp/*.bin
+mkdir -p /tmp/wr841temp
+cd /tmp/wr841temp
+wget "https://static.tp-link.com/res/down/soft/TL-WR841N_V10_150310.zip"
+unzip -j TL-WR841*.zip
+rm TL-WR841*.zip
+echo "Hersteller-Firmware erfolgreich heruntergeladen und entpackt"
+mv wr841*.bin orig.bin
+dd if=orig.bin of=wr841nv10_tp_recovery.bin skip=257 bs=512
+cp wr841nv10_tp_recovery.bin /srv/tftp/
+echo
+echo "1. Verbinden Sie nun die blaue WAN-Buchse des Routers mit dem Netzwerk."
+echo "2. Schalten Sie den Router aus."
+echo "3. Schalten Sie den Router bei gedrückter Reset-Taste drücken) ein."
+echo "4. Warten Sie ca. 5 Sekunden bis Sie die Reset-Taste loslassen."
+echo "5. Der Router sollte nach ca. 1 Minute neu starten und funktionsfähig sein."
+echo
+read -p "Bitte ENTER drücken um fortzufahren."
+clear
+auswahl
+}
+
+wr841v11ff() {
+ifconfig eth0:0 192.168.0.66 netmask 255.255.255.0 up
+systemctl restart tftpd-hpa
+rm -rf /tmp/*
+rm -rf /srv/tftp/*.bin
+mkdir -p /tmp/wr841temp
+cd /tmp/wr841temp
+wget "https://wizard.freifunk-lippe.de/images/stable/factory/gluon-fflip-1.1.3-20190502-tp-link-tl-wr841n-nd-v11.bin"
+echo "Freifunk-Firmware erfolgreich heruntergeladen"
+cp gluon-fflip*.bin /srv/tftp/wr841nv11_tp_recovery.bin
+echo
+echo "1. Verbinden Sie nun die blaue WAN-Buchse des Routers mit dem Netzwerk."
+echo "2. Schalten Sie den Router aus."
+echo "3. Schalten Sie den Router bei gedrückter Reset-Taste ein."
+echo "4. Warten Sie ca. 5 Sekunden bis Sie die Reset-Taste loslassen."
+echo "5. Der Router sollte nach ca. 1 Minute neu starten und funktionsfähig sein."
+echo
+read -p "Bitte ENTER drücken um fortzufahren."
+clear
+auswahl
+}
+
+wr841v11stock() {
+ifconfig eth0:0 192.168.0.66 netmask 255.255.255.0 up
+systemctl restart tftpd-hpa
+rm -rf /tmp/*
+rm -rf /srv/tftp/*.bin
+mkdir -p /tmp/wr841temp
+cd /tmp/wr841temp
+wget "https://static.tp-link.com/TL-WR841N(EU)_V11_160325_1473219209872x.zip"
+unzip -j TL-WR841*.zip
+rm TL-WR841*.zip
+echo "Hersteller-Firmware erfolgreich heruntergeladen und entpackt"
+mv wr841*.bin orig.bin
+dd if=orig.bin of=wr841nv11_tp_recovery.bin skip=257 bs=512
+cp wr841nv11_tp_recovery.bin /srv/tftp/
+echo
+echo "1. Verbinden Sie nun die blaue WAN-Buchse des Routers mit dem Netzwerk."
+echo "2. Schalten Sie den Router aus."
+echo "3. Schalten Sie den Router bei gedrückter Reset-Taste drücken) ein."
+echo "4. Warten Sie ca. 5 Sekunden bis Sie die Reset-Taste loslassen."
+echo "5. Der Router sollte nach ca. 1 Minute neu starten und funktionsfähig sein."
+echo
+read -p "Bitte ENTER drücken um fortzufahren."
+clear
+auswahl
+}
+
+wr841v12ff() {
+ifconfig eth0:0 192.168.0.66 netmask 255.255.255.0 up
+systemctl restart tftpd-hpa
+rm -rf /tmp/*
+rm -rf /srv/tftp/*.bin
+mkdir -p /tmp/wr841temp
+cd /tmp/wr841temp
+wget "https://wizard.freifunk-lippe.de/images/stable/factory/gluon-fflip-1.1.3-20190502-tp-link-tl-wr841n-nd-v12.bin"
+echo "Freifunk-Firmware erfolgreich heruntergeladen"
+cp gluon-fflip*.bin /srv/tftp/wr841nv11_tp_recovery.bin
+echo
+echo "1. Verbinden Sie nun die blaue WAN-Buchse des Routers mit dem Netzwerk."
+echo "2. Schalten Sie den Router aus."
+echo "3. Schalten Sie den Router bei gedrückter Reset-Taste ein."
+echo "4. Warten Sie ca. 5 Sekunden bis Sie die Reset-Taste loslassen."
+echo "5. Der Router sollte nach ca. 1 Minute neu starten und funktionsfähig sein."
+echo
+read -p "Bitte ENTER drücken um fortzufahren."
+clear
+auswahl
+}
+
+wr841v12stock() {
+ifconfig eth0:0 192.168.0.66 netmask 255.255.255.0 up
+systemctl restart tftpd-hpa
+rm -rf /tmp/*
+rm -rf /srv/tftp/*.bin
+mkdir -p /tmp/wr841temp
+cd /tmp/wr841temp
+wget "https://static.tp-link.com/TL-WR841N(EU)_V12_160624_1476692106947j.zip"
+unzip -j TL-WR841*.zip
+rm TL-WR841*.zip
+echo "Hersteller-Firmware erfolgreich heruntergeladen und entpackt"
+mv wr841*.bin orig.bin
+dd if=orig.bin of=wr841nv11_tp_recovery.bin skip=257 bs=512
+cp wr841nv11_tp_recovery.bin /srv/tftp/
+echo
+echo "1. Verbinden Sie nun die blaue WAN-Buchse des Routers mit dem Netzwerk."
+echo "2. Schalten Sie den Router aus."
+echo "3. Schalten Sie den Router bei gedrückter Reset-Taste drücken) ein."
+echo "4. Warten Sie ca. 5 Sekunden bis Sie die Reset-Taste loslassen."
+echo "5. Der Router sollte nach ca. 1 Minute neu starten und funktionsfähig sein."
+echo
+read -p "Bitte ENTER drücken um fortzufahren."
+clear
+auswahl
+}
+
 
 archerc50v3ff() {
 ifconfig eth0:0 192.168.0.66 netmask 255.255.255.0 up
@@ -156,15 +397,65 @@ auswahl
 
 auswahl() {
  cd
- echo "D E B R I C K E R"
- echo "Rettungs-Script für gebrickte Router.   Folgen Sie bitte den Anweisungen."
- echo "Während der Ausführung des Scipts wird eine Internet-Verbindung benötigt."
+ echo "--- D E B R I C K E R ---"
+ echo "Rettungs-Script für gebrickte Router - folgen Sie bitte den Anweisungen."
+ echo "Während der Ausführung des Scripts wird eine Internet-Verbindung benötigt."
  echo
  PS3='Bitte Routermodell auswählen: '
- options=("TP-Link Archer C50 v3 Freifunk" "TP-Link Archer C50 v3 Hersteller" "TP-Link Archer C50 v4 Freifunk" "TP-Link Archer C50 v4 Hersteller" "Beenden")
+ options=("TP-Link TL-WR841 v8 Freifunk" "TP-Link TL-WR841 v8 Hersteller" "TP-Link TL-WR841 v9 Freifunk" "TP-Link TL-WR841 v9 Hersteller" "TP-Link TL-WR841 v10 Freifunk" "TP-Link TL-WR841 v10 Hersteller" "TP-Link TL-WR841 v11 Freifunk" "TP-Link TL-WR841 v11 Hersteller" "TP-Link TL-WR841 v12 Freifunk" "TP-Link TL-WR841 v12 Hersteller" "TP-Link Archer C50 v3 Freifunk" "TP-Link Archer C50 v3 Hersteller" "TP-Link Archer C50 v4 Freifunk" "TP-Link Archer C50 v4 Hersteller" "Beenden")
  select opt in "${options[@]}"
  do
      case $opt in
+         "TP-Link TL-WR841 v8 Freifunk")
+              echo "Du hast TP-Link TL-WR841 v8 Freifunkk ausgewählt. Weiter geht's..."
+              wr841v8ff
+              exit
+              ;;
+         "TP-Link TL-WR841 v8 Hersteller")
+              echo "Du hast TP-Link TL-WR841 v8 Hersteller ausgewählt. Weiter geht's..."
+              wr841v8stock
+              exit
+              ;;
+         "TP-Link TL-WR841 v9 Freifunk")
+              echo "Du hast TP-Link TL-WR841 v9 Freifunkk ausgewählt. Weiter geht's..."
+              wr841v9ff
+              exit
+              ;;
+         "TP-Link TL-WR841 v9 Hersteller")
+              echo "Du hast TP-Link TL-WR841 v9 Hersteller ausgewählt. Weiter geht's..."
+              wr841v9stock
+              exit
+              ;;
+         "TP-Link TL-WR841 v10 Freifunk")
+              echo "Du hast TP-Link TL-WR841 v10 Freifunkk ausgewählt. Weiter geht's..."
+              wr841v10ff
+              exit
+              ;;
+         "TP-Link TL-WR841 v10 Hersteller")
+              echo "Du hast TP-Link TL-WR841 v10 Hersteller ausgewählt. Weiter geht's..."
+              wr841v10stock
+              exit
+              ;;
+         "TP-Link TL-WR841 v11 Freifunk")
+              echo "Du hast TP-Link TL-WR841 v11 Freifunkk ausgewählt. Weiter geht's..."
+              wr841v11ff
+              exit
+              ;;
+         "TP-Link TL-WR841 v11 Hersteller")
+              echo "Du hast TP-Link TL-WR841 v11 Hersteller ausgewählt. Weiter geht's..."
+              wr841v11stock
+              exit
+              ;;
+         "TP-Link TL-WR841 v12 Freifunk")
+              echo "Du hast TP-Link TL-WR841 v12 Freifunkk ausgewählt. Weiter geht's..."
+              wr841v12ff
+              exit
+              ;;
+         "TP-Link TL-WR841 v12 Hersteller")
+              echo "Du hast TP-Link TL-WR841 v12 Hersteller ausgewählt. Weiter geht's..."
+              wr841v12stock
+              exit
+              ;;
          "TP-Link Archer C50 v3 Freifunk")
               echo "Du hast TP-Link Archer C50 v3 Freifunk ausgewählt. Weiter geht's..."
               archerc50v3ff
